68f73140510d8ee548f6970430e1cbac4c2d5b4a
Soyl <soyl@live.cn>
2020-02-28 21:55:49 +0800

    更新

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,10 @@
-import FluentSQLite
+import FluentMySQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentMySQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
